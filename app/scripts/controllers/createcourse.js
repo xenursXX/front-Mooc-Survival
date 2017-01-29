da78293f39ab767dfbe01711b4f39afde38fb18d
guillaume nouhaud <guillaumenouhaud@macbook-pro-de-guillaume-4.home>
2017-01-29 19:53:35 +0100

    page create course

--- app/scripts/controllers/createcourse.js
+++ app/scripts/controllers/createcourse.js
@@ -11,7 +11,15 @@ angular.module('frontMoocSurvivalApp')
   .controller('CreateCourseCtrl', function ($resource, $routeParams, CoursesService, $location, FileUploader) {
 
     this.uploader = new FileUploader();
+    this.arrayQuestions = [1,2];
+    this.displayQuestion = true;
 
+    this.addQuestion = function(){
+      this.arrayQuestions.push(+1);
+      if(this.arrayQuestions.length > 19){
+        this.displayQuestion = false;
+      }
+    }
     tinymce.init({
       selector: 'textarea',  // change this value according to your HTML
       plugins: ["image link save emoticons code colorpicker fullscreen media"],
@@ -26,7 +34,5 @@ angular.module('frontMoocSurvivalApp')
       SendDataForm.push(dataForm);
       console.log('SendDataForm',dataForm);
     }
-
-
   }
 );
