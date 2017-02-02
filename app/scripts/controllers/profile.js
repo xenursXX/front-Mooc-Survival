7e42473bf641d29a0c86cc0f789a06d95ea9af4e
fabmlt <fabmontillot@gmail.com>
2017-02-02 22:55:22 +0100

    Some fix

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -8,18 +8,31 @@
  * Controller of the frontMoocSurvivalApp
  */
 angular.module('frontMoocSurvivalApp')
-  .controller('ProfileCtrl', function (UserService, $routeParams, Restangular, $location, $scope) {
+  .controller('ProfileCtrl', function (UserService, $routeParams, Restangular, $location, $scope, ngDialog) {
     // this.userProfile = UserService.getUserData();
 
     // console.log(this.userProfile);
 
-    Restangular.one('users', $routeParams.idprofile).get().then(function (data) {
+    
+
+    if(!UserService.getUserData()) {
+      $location.path('/404');
+      ngDialog.open({
+          template: '<p class="text-center">Il faut être connecté</p>',
+          plain: true
+      });
+      return;
+    }
+
+    var userId = UserService.getUserData().id;
+
+    Restangular.one('users', $routeParams.idprofile).get({}, { Authorization: 'JWT ' + localStorage.getItem("token") }).then(function (data) {
   		$scope.userProfile = data.plain();
   	}, function (error) {
   		$location.path('/404');
   	})
 
-    var userId = UserService.getUserData().id
+    
 
     $scope.isMe = ( userId == $routeParams.idprofile) ? true : false;
 
@@ -31,11 +44,34 @@ angular.module('frontMoocSurvivalApp')
       for(var i = 0; i < data.length; i++) {
         for(var j = 0; j < data[i].steps.length; j++){
           if(data[i].steps[j].student.id == userId) {
+            //console.log(data[i].quizzes[0].id);
+            // data[i].steps[j].result = {};
+            
+            getScore(data[i].quizzes[0].id, j);
             $scope.coursesListStudent.push(data[i].steps[j]);
+            //console.log(data[i].steps[j]);
           }
         }
       }
     });
+
+    var getScore = function (id_quizz, key) {
+      Restangular.one('quizzes', id_quizz).getList('scores').then(function (data) {
+        // Find my score
+        for (var i = 0; i < data.length; i++) {
+          if(data[i].student.id == userId) {
+
+            Restangular.one('quizzes', id_quizz).one('scores', data[i].id).get().then(function (data2) {
+              if(data2.max_score){
+                $scope.coursesListStudent[key].result = data2.plain();
+              }
+            })
+
+            break;
+          }
+        }
+      })
+    }
 
   }
 );
